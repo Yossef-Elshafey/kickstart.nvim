ffe53780c29492348c5a445fbfed720943c662c5
Yossef Elshafey <yssef0472@gmail.com>
2026-06-03 10:30:12 +0300

    remove color picker function most of colorscheme is not installed

--- lua/custom/plugins/options.lua
+++ lua/custom/plugins/options.lua
@@ -17,31 +17,6 @@ vim.keymap.set('v', '<C-l>', '!column -t<CR>')
 vim.keymap.set('n', '<C-x>', ':bdelete<CR>')
 vim.api.nvim_create_user_command('W', 'w', {})
 
-local function colorPicker()
-  local colors = {
-    'habamax',
-    'ayu-dark',
-    'ayu',
-    'ayu-mirage',
-    'catppuccin',
-    'lunaperche',
-    'randomhue',
-    'retrobox',
-    'tokyonight-moon',
-    'carvion',
-  }
-  for index, color in ipairs(colors) do
-    print(index, color)
-  end
-
-  local color = tonumber(vim.fn.input 'Choose color: ')
-  local colorname = colors[color]
-  local cmd = 'colorscheme' .. ' ' .. colorname
-  vim.cmd(cmd)
-end
-
-vim.api.nvim_create_user_command('ColorPicker', colorPicker, { nargs = 0, desc = 'Set Colorscheme' })
-
 local function transparent_background()
   vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
   -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
